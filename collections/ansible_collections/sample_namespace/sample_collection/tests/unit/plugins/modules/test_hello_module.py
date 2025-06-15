f4d59ecc72b26d0dbb8af295ab82f47e223b0840
gitea <gitea@gitea.com>
2025-06-15 09:25:22 +0200

    Sanity fixes

--- collections/ansible_collections/sample_namespace/sample_collection/tests/unit/plugins/modules/test_hello_module.py
+++ collections/ansible_collections/sample_namespace/sample_collection/tests/unit/plugins/modules/test_hello_module.py
@@ -2,7 +2,11 @@ from unittest.mock import patch
 
 # Replace 'sample_namespace.sample_collection.plugins.modules.hello_module'
 # with the actual import path to your module.
+
+
 @patch("sample_namespace.sample_collection.plugins.modules.hello_module.AnsibleModule")
+
+
 def test_hello_module_main(mock_ansible_module):
     # Setup mock
     instance = mock_ansible_module.return_value
