3916e93dc4aaecafa6159d3ccff39508a2bf34ed
gitea <gitea@gitea.com>
2025-06-19 09:04:52 +0200

    Pus new content

---
+++ collections/ansible_collections/sample_namespace/sample_collection/.ansible/collections/ansible_collections/sample_namespace/sample_collection/tests/unit/plugins/modules/test_hello_module.py
@@ -0,0 +1,20 @@
+from unittest.mock import patch
+
+# Replace 'sample_namespace.sample_collection.plugins.modules.hello_module'
+# with the actual import path to your module.
+
+
+@patch("sample_namespace.sample_collection.plugins.modules.hello_module.AnsibleModule")
+def test_hello_module_main(mock_ansible_module):
+    # Setup mock
+    instance = mock_ansible_module.return_value
+    instance.params = {"name": "World"}
+
+    # Import your module under test
+    from sample_namespace.sample_collection.plugins.modules import hello_module
+
+    # Run the main function
+    hello_module.main()
+
+    # Check expected behavior
+    instance.exit_json.assert_called_once_with(changed=False, message="Hello, World!")
